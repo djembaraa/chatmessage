a1fb6ae6100b328a90c50e4f925a7c48c2d45cdc
djembar <djembararafat98@gmail.com>
2026-07-20 17:18:03 +0700

    feat: establish database connection on server start

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT
 
 
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+app.listen(PORT, () => {
+    connectDB(); // Call the connectDB function to establish a connection to the database
+    console.log(`Server is up and running on port ${PORT}`)
+});
