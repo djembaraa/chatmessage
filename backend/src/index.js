a90acae49bacf15aff1fe48b34b840edc8f58ca7
djembar <djembararafat98@gmail.com>
2026-07-28 16:48:31 +0700

    feat: add CORS and Clerk middleware for enhanced security and frontend integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,26 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+
+import { clerkMiddleware } from "@clerk/express";
+
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+}));
+app.use(clerkMiddleware( ))
 
 // test route to check if the server is running restfully
 app.get("/health", (req, res) => {
+
     res.status(200).json({ status: "OK", message: "Server is running smoothly" });
 });
 
