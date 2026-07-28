b47e4b894d783ef40e5b1fe2ee38cf23a0daae45
djembar <djembararafat98@gmail.com>
2026-07-28 22:16:42 +0700

    fix: update import statement for job module in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
-import job from "./lib/cron.js";
+import { job } from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
