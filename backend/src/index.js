8817901ff7106b3a0a523c06bd9f4ea4f4012035
djembar <djembararafat98@gmail.com>
2026-07-28 17:48:56 +0700

    feat: add static file serving for frontend assets

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 
+import fs from "fs";
+import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
@@ -10,6 +13,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({
@@ -23,6 +28,14 @@ app.get("/health", (req, res) => {
 
     res.status(200).json({ status: "OK", message: "Server is running smoothly" });
 });
+
+if(fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB(); // Call the connectDB function to establish a connection to the database
