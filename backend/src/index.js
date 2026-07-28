7a27955769f47a921e9d2e07702caa854de469a7
djembar <djembararafat98@gmail.com>
2026-07-28 22:09:22 +0700

    refactor: remove unnecessary blank lines in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,6 @@ import { connectDB } from "./lib/db.js";
 import job from "./cron/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js";
 
-
-
 const app = express();
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL;
